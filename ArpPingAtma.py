94c4dca45cb46aae78b19926367f24b68243fbed
Bahadır Bağoğlu <bahadirbagoglu1420@gmail.com>
2022-09-19 15:56:08 +0300

    Create ArpPingAtma.py
    The process of throwing ARPping to the other party with the IP requested from the user

---
+++ ArpPingAtma.py
@@ -0,0 +1,40 @@
+
+Kullanıcıdan istenile IP ile karşı tarafa ARPpingi atma işlemi ///  The process of throwing ARPping to the other party with the IP requested from the user
+
+import argparse
+import scapy.all as scapy
+
+class ARPPing():
+
+	def __init__(self):
+		print("ARP işlemi başlatıldı")
+
+	def get_user_input(self):
+		parser = argparse.ArgumentParser()
+
+		parser.add_argument('-i', '--ipaddress', type=str, help="ARP pingi göndermek için ip aralığını girin")
+		args = parser.parse_args()
+		print(args.ipaddress)
+
+		if args.ipaddress != None:
+			return args
+		else:
+			print("IP aralığını girin!")
+
+
+	def execute_arp(self,ip):
+
+		arp_request_packet = scapy.ARP(pdst=ip)
+
+		broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+		combined_packet = broadcast_packet/arp_request_packet
+
+		(answered_list, unanswered_list) = scapy.srp(combined_packet,timeout=1)
+
+		answered_list.summary()
+
+if __name__ == "__main__":
+	arp_ping = ARPPing()
+	ip_range = arp_ping.get_user_input()
+	arp_ping.execute_arp(ip_range.ipaddress)
